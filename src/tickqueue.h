fa72ba8b3e3f71396bf376e8fc9fafa54c2a3bd9
mengcz <mengcz1126@gmail.com>
2024-07-01 13:30:44 +0800

    优化TickQueue实现，循环队列

--- src/tickqueue.h
+++ src/tickqueue.h
@@ -66,6 +66,19 @@ protected:
     std::list<T> q;
 };
 
+// template<typename T>
+// class LimitedTickVector {
+// public:
+//     LimitedTickList(uint32_t input_wid, uint32_t queue_size) {
+
+//     }
+
+// protected:
+//     uint32_t iwid, qsize;
+//     std::vector<T> push_buf;
+//     std::vector<T> q;
+// };
+
 /**
  * 延迟1周期写入的列表结构，写入的项会在下一周期进入主列表
 */
@@ -206,9 +219,7 @@ protected:
 
 template <typename T>
 class SimpleTickQueue : public SimObject {
-
 public:
-
     SimpleTickQueue(uint32_t input_wid, uint32_t output_wid, uint32_t queue_size) {
         assert(input_wid > 0);
         assert(output_wid > 0);
@@ -217,179 +228,187 @@ public:
         iwid = input_wid;
         owid = output_wid;
         qsize = queue_size;
+        len = ALIGN((iwid + owid + qsize) * 2, 16);
+        bottom = q_bottom = push_bottom = ptr = pop_sz = q_sz = push_sz = 0;
+        buf.resize(len);
         do_on_current_tick = 0;
         do_apply_next_tick = 1;
     }
-    ~SimpleTickQueue() {}
     virtual void apply_next_tick() {
-        while(pop_buf.size() < owid && !q.empty()) pop_buf.splice(pop_buf.end(), q, q.begin());
-        while(pop_buf.size() < owid && !push_buf.empty()) pop_buf.splice(pop_buf.end(), push_buf, push_buf.begin());
-        while(q.size() < qsize && !push_buf.empty()) q.splice(q.end(), push_buf, push_buf.begin());
+        if(qsize) {
+            uint32_t q2pop = std::min<uint32_t>(owid - pop_sz, q_sz);
+            q_bottom = (q_bottom + q2pop) % len;
+            pop_sz += q2pop;
+            q_sz -= q2pop;
+        }
+        {
+            uint32_t push2pop = std::min<uint32_t>(owid - pop_sz, push_sz);
+            q_bottom = (q_bottom + push2pop) % len;
+            push_bottom = (push_bottom + push2pop) % len;
+            pop_sz += push2pop;
+            push_sz -= push2pop;
+        }
+        if(qsize) {
+            uint32_t push2q = std::min<uint32_t>(qsize - q_sz, push_sz);
+            push_bottom = (push_bottom + push2q) % len;
+            q_sz += push2q;
+            push_sz -= push2q;
+        }
     }
 
     inline uint32_t input_wid() {return iwid;}
     inline uint32_t output_wid() {return owid;}
-    inline uint32_t can_push() {return ((push_buf.size() < iwid)?(iwid - push_buf.size()):0);}
-    inline uint32_t can_pop() {return std::min<uint32_t>(owid, pop_buf.size());}
+    inline uint32_t can_push() {return iwid - push_sz;}
+    inline uint32_t can_pop() {return pop_sz;}
 
-    inline bool push(T &v) {
-        if(push_buf.size() >= iwid) return false;
-        push_buf.emplace_back(v);
-        return true;
-    }
     inline T& top() {
-        return pop_buf.front();
+        if(pop_sz == 0) [[unlikely]] assert(0);
+        return buf[bottom];
     }
+
     inline void pop() {
-        pop_buf.pop_front();
+        if(pop_sz == 0) [[unlikely]] assert(0);
+        bottom = (bottom + 1) % len;
+        pop_sz--;
+    }
+    inline bool push(T &v) {
+        if(push_sz >= iwid) [[unlikely]] return false;
+        buf[ptr] = v;
+        ptr = (ptr + 1) % len;
+        push_sz++;
+        return true;
     }
 
     inline void clear(std::list<T> *out = nullptr) {
         if(out) {
-            out->splice(out->end(), pop_buf);
-            out->splice(out->end(), q);
-            out->splice(out->end(), push_buf);
+            for(uint32_t i = bottom ; i != ptr; i = (i+1)%len) {
+                out->push_back(buf[i]);
+            }
         }
-        push_buf.clear();
-        q.clear();
-        pop_buf.clear();
+        bottom = q_bottom = push_bottom = ptr = pop_sz = q_sz = push_sz = 0;
     }
 
     inline bool empty() {
-        return (push_buf.empty() && pop_buf.empty() && q.empty());
+        return (bottom == ptr);
     }
 
     inline bool pass_to(SimpleTickQueue<T> &q2, uint32_t cnt = 1) {
         if(cnt == 0) return true;
         if(can_pop() < cnt || q2.can_push() < cnt) return false;
-        auto it = pop_buf.begin();
-        std::advance(it, cnt);
-        q2.push_buf.splice(q2.push_buf.end(), pop_buf, pop_buf.begin(), it);
+        for(uint32_t i = 0; i < cnt; i++) {
+            q2.push(top());
+            pop();
+        }
         return true;
     }
 
     inline uint64_t size() {
-        return (push_buf.size() + q.size() + pop_buf.size());
+        return (push_sz + pop_sz + q_sz);
     }
     inline uint64_t cur_size() {
-        return (q.size() + pop_buf.size());
+        return (pop_sz + q_sz);
     }
 
     inline void dbg_get_all(std::vector<T> *out) {
-        out->reserve(push_buf.size() + q.size() + pop_buf.size());
-        for(auto &e : push_buf) out->emplace_back(e);
-        for(auto &e : q) out->emplace_back(e);
-        for(auto &e : pop_buf) out->emplace_back(e);
+        out->reserve(size());
+        for(uint32_t i = bottom ; i != ptr; i = (i+1)%len) {
+            out->push_back(buf[i]);
+        }
     }
 
 protected:
     uint32_t iwid, owid, qsize;
-    std::list<T> push_buf;
-    std::list<T> pop_buf;
-    std::list<T> q;
+    uint32_t len, bottom, q_bottom, push_bottom, ptr;
+    uint32_t pop_sz, q_sz, push_sz;
+    std::vector<T> buf;
 };
 
-// template <typename T, int IWid = 1, int OWid = 1, int QSize = 1>
-// class OneTickQueue : public SimObject {
+// template <typename T>
+// class SimpleTickQueue : public SimObject {
+
 // public:
-//     OneTickQueue() {
-//         static_assert(IWid > 0);
-//         static_assert(OWid > 0);
-//         static_assert(QSize > 0);
-//         static_assert(QSize >= IWid);
-//         static_assert(QSize >= OWid);
+
+//     SimpleTickQueue(uint32_t input_wid, uint32_t output_wid, uint32_t queue_size) {
+//         assert(input_wid > 0);
+//         assert(output_wid > 0);
+//         assert(input_wid < (1<<30));
+//         assert(output_wid < (1<<30));
+//         iwid = input_wid;
+//         owid = output_wid;
+//         qsize = queue_size;
 //         do_on_current_tick = 0;
 //         do_apply_next_tick = 1;
 //     }
-
+//     ~SimpleTickQueue() {}
 //     virtual void apply_next_tick() {
-//         lock.lock();
-//         to_push_cnt = std::min<uint32_t>(to_push_cnt, IWid);
-//         to_pop_cnt = std::min<uint32_t>(to_pop_cnt, OWid);
-//         to_pop_cnt = std::min<uint32_t>(to_pop_cnt, q.size());
-//         uint32_t wb_cnt = std::min<uint32_t>(OWid, q.size());
-//         for(uint32_t i = 0; i < to_pop_cnt; i++) {
-//             q.pop_front();
-//         }
-//         {
-//             auto iter = q.begin();
-//             for(uint32_t i = to_pop_cnt; i < wb_cnt; i++, iter++) {
-//                 *iter = top_buf[i];
-//             }
-//         }
-//         for(uint32_t i = 0; i < to_push_cnt; i++) {
-//             q.push_back(push_buf[i]);
-//         }
-//         uint32_t can_pop = std::min<uint32_t>(OWid, q.size());
-//         auto iter = q.begin();
-//         for(uint32_t i = 0; i < can_pop; i++, iter++) {
-//             top_buf[i] = (*iter);
-//         }
-//         to_push_cnt = to_pop_cnt = 0;
-//         if(q.size() > QSize) {
-//             to_push_cnt = q.size() - QSize;
-//             for(uint32_t i = to_push_cnt - 1; 1; i--) {
-//                 push_buf[i] = q.back();
-//                 q.pop_back();
-//                 if(i == 0) break;
-//             }
-//         }
-//         lock.unlock();
-//     };
-    
-//     inline uint32_t input_width() {
-//         return IWid;
+//         if(qsize) while(pop_buf.size() < owid && !q.empty()) pop_buf.splice(pop_buf.end(), q, q.begin());
+//         while(pop_buf.size() < owid && !push_buf.empty()) pop_buf.splice(pop_buf.end(), push_buf, push_buf.begin());
+//         if(qsize) while(q.size() < qsize && !push_buf.empty()) q.splice(q.end(), push_buf, push_buf.begin());
 //     }
-//     inline uint32_t can_push_cnt() {
-//         lock.lock();
-//         uint32_t ret = ((to_push_cnt < IWid)?(IWid - to_push_cnt):0);
-//         lock.unlock();
-//         return ret;
-//     }
-//     inline void push(T &v) {
-//         lock.lock();
-//         if(to_push_cnt < IWid) {
-//             push_buf[to_push_cnt] = v;
-//             to_push_cnt++;
-//         }
-//         lock.unlock();
-//     }
-//     uint32_t to_push_cnt = 0;
-//     T push_buf[IWid];
 
-//     inline uint32_t output_width() {
-//         return OWid;
-//     }
-//     inline uint32_t can_pop_cnt() {
-//         lock.lock();
-//         uint32_t ret = std::min<uint32_t>(OWid, q.size());
-//         lock.unlock();
-//         return ret;
-//     };
-//     inline void pop(uint32_t cnt = 1) {
-//         lock.lock();
-//         to_pop_cnt = std::min<uint32_t>(OWid, to_pop_cnt + cnt);
-//         lock.unlock();
-//     };
-//     T top_buf[OWid];
-//     uint32_t to_pop_cnt = 0;
+//     inline uint32_t input_wid() {return iwid;}
+//     inline uint32_t output_wid() {return owid;}
+//     inline uint32_t can_push() {return ((push_buf.size() < iwid)?(iwid - push_buf.size()):0);}
+//     inline uint32_t can_pop() {return std::min<uint32_t>(owid, pop_buf.size());}
 
-//     void clear() {
-//         lock.lock();
+//     inline bool push(T &v) {
+//         if(push_buf.size() >= iwid) return false;
+//         push_buf.emplace_back(v);
+//         return true;
+//     }
+//     inline T& top() {
+//         return pop_buf.front();
+//     }
+//     inline void pop() {
+//         pop_buf.pop_front();
+//     }
+
+//     inline void clear(std::list<T> *out = nullptr) {
+//         if(out) {
+//             out->splice(out->end(), pop_buf);
+//             out->splice(out->end(), q);
+//             out->splice(out->end(), push_buf);
+//         }
+//         push_buf.clear();
 //         q.clear();
-//         to_push_cnt = to_pop_cnt = 0;
-//         lock.unlock();
-//     };
+//         pop_buf.clear();
+//     }
 
-//     void debug_get_queue(std::list<T> *out) {
-//         out->clear();
-//         out->assign(q.begin(), q.end());
+//     inline bool empty() {
+//         return (push_buf.empty() && pop_buf.empty() && q.empty());
+//     }
+
+//     inline bool pass_to(SimpleTickQueue<T> &q2, uint32_t cnt = 1) {
+//         if(cnt == 0) return true;
+//         if(can_pop() < cnt || q2.can_push() < cnt) return false;
+//         auto it = pop_buf.begin();
+//         std::advance(it, cnt);
+//         q2.push_buf.splice(q2.push_buf.end(), pop_buf, pop_buf.begin(), it);
+//         return true;
+//     }
+
+//     inline uint64_t size() {
+//         return (push_buf.size() + q.size() + pop_buf.size());
+//     }
+//     inline uint64_t cur_size() {
+//         return (q.size() + pop_buf.size());
+//     }
+
+//     inline void dbg_get_all(std::vector<T> *out) {
+//         out->reserve(push_buf.size() + q.size() + pop_buf.size());
+//         for(auto &e : push_buf) out->emplace_back(e);
+//         for(auto &e : q) out->emplace_back(e);
+//         for(auto &e : pop_buf) out->emplace_back(e);
 //     }
 
 // protected:
-//     SpinLock lock;
+//     uint32_t iwid, owid, qsize;
+//     std::list<T> push_buf;
+//     std::list<T> pop_buf;
 //     std::list<T> q;
 // };
+
+
 
 namespace test {
 
